1aad8877ea777abf21b030c1ab347600420e8fa0
Peter Winton <wintonpc@gmail.com>
2013-03-17 14:07:46 -0400

    Refactored RNN tests a little

--- QuqeTest/RNNTests.cs
+++ QuqeTest/RNNTests.cs
@@ -27,80 +27,47 @@ namespace QuqeTest
     [Test]
     public void RNNReproduceability()
     {
-      Func<DateTime, DateTime, PreprocessedData> getData = (start, end) =>
-        Versace.GetPreprocessedValues(PreprocessingType.Enhanced, "DIA", start, end, true, Versace.GetIdealSignalFunc(PredictionType.NextClose));
+      var data = GetData("2004-01-01", "2004-05-01");
 
-      var data = getData(DateTime.Parse("2004-01-01"), DateTime.Parse("2004-05-01"));
-
-      var numInputs = data.Inputs.RowCount;
-      var layers = new List<LayerSpec> {
-        new LayerSpec { NodeCount = 8, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
-        new LayerSpec { NodeCount = 4, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
-        new LayerSpec { NodeCount = 1, IsRecurrent = false, ActivationType = ActivationType.Linear }
-      };
-
-      QuqeUtil.Random = new Random(42);
-
-      var weightCount = RNN.GetWeightCount(layers, numInputs);
-      var initialWeights = QuqeUtil.MakeRandomVector(weightCount, -1, 1);
-
-      var checksums = new HashSet<string>();
-      List.Repeat(5, i => {
+      var checksums = List.Repeat(5, i => {
         QuqeUtil.Random = new Random(42);
-        var trainResult = RNN.TrainSCG(layers, initialWeights, 1000, data.Inputs, data.Outputs);
+        var trainResult = Train(data, 8, 4, 1000);
         var ws = trainResult.RNNSpec.Weights;
-        checksums.Add(Checksum(trainResult.RNNSpec.Weights));
+        return Checksum(trainResult.RNNSpec.Weights);
       });
-      checksums.Count.ShouldEqual(1);
+      checksums.Distinct().Count().ShouldEqual(1);
     }
 
     [Test]
-    [STAThread]
     public void RNNPredictsWell()
     {
-      Func<DateTime, DateTime, PreprocessedData> getData = (start, end) =>
-        Versace.GetPreprocessedValues(PreprocessingType.Enhanced, "DIA", start, end, true, Versace.GetIdealSignalFunc(PredictionType.NextClose));
+      QuqeUtil.Random = new Random(42);
+      var trainingData = GetData("2004-01-01", "2004-07-01");
+      var trainResult = Train(trainingData, 8, 4, 1000);
+      var trainingFitness = VMixture.ComputePredictorFitness(new RNN(trainResult.RNNSpec), trainingData.Inputs, trainingData.Outputs);
+      
+      Trace.WriteLine("Training fitness: " + trainingFitness);
+      trainingFitness.ShouldEqual(0.984);
+    }
 
-      var trainingData = getData(DateTime.Parse("2004-01-01"), DateTime.Parse("2006-01-01"));
-      var testData = getData(DateTime.Parse("2006-01-01"), DateTime.Parse("2006-06-01"));
+    static PreprocessedData GetData(string startDate, string endDate)
+    {
+      return Versace.GetPreprocessedValues(PreprocessingType.Enhanced, "DIA", DateTime.Parse(startDate), DateTime.Parse(endDate),
+        true, Versace.GetIdealSignalFunc(PredictionType.NextClose));
+    }
 
+    static RnnTrainResult Train(PreprocessedData trainingData, int layer1NodeCount, int layer2NodeCount, int epochMax)
+    {
       var numInputs = trainingData.Inputs.RowCount;
       var layers = new List<LayerSpec> {
-        new LayerSpec { NodeCount = 12, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
-        new LayerSpec { NodeCount = 5, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
+        new LayerSpec { NodeCount = layer1NodeCount, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
+        new LayerSpec { NodeCount = layer2NodeCount, IsRecurrent = true, ActivationType = ActivationType.LogisticSigmoid },
         new LayerSpec { NodeCount = 1, IsRecurrent = false, ActivationType = ActivationType.Linear }
       };
 
-      QuqeUtil.Random = new Random(42);
-
       var weightCount = RNN.GetWeightCount(layers, numInputs);
       var initialWeights = QuqeUtil.MakeRandomVector(weightCount, -1, 1);
-      var trainResult = RNN.TrainSCG(layers, initialWeights, 1000, trainingData.Inputs, trainingData.Outputs);
-      var rnnSpec = trainResult.RNNSpec;
-
-      var net = new RNN(rnnSpec);
-      var trainingFitness = VMixture.ComputePredictorFitness(net, trainingData.Inputs, trainingData.Outputs);
-      //var testFitness = VMixture.ComputePredictorFitness(net, testData.Inputs, testData.Outputs);
-
-      var trainingFitnessAtEachEpoch = trainResult.WeightHistory.Select((w, i) =>
-        VMixture.ComputePredictorFitness(new RNN(new RNNSpec(rnnSpec.NumInputs, rnnSpec.Layers, w)),
-        trainingData.Inputs, trainingData.Outputs)).ToList();
-
-      var testingFitnessAtEachEpoch = trainResult.WeightHistory.Select((w, i) =>
-        VMixture.ComputePredictorFitness(new RNN(new RNNSpec(rnnSpec.NumInputs, rnnSpec.Layers, w)),
-        testData.Inputs, testData.Outputs)).ToList();
-
-      var plot = new EqPlotWindow();
-      plot.Show();
-      plot.ThePlot.DrawLineGraph(List.Create(
-        new PlotDesc { ys = trainResult.CostHistory.Skip(1).ToList(), Color = Colors.Blue },
-        new PlotDesc { ys = trainingFitnessAtEachEpoch.Select(x => -x).Skip(1).ToList(), Color = Colors.Green },
-        new PlotDesc { ys = testingFitnessAtEachEpoch.Select(x => -x).Skip(1).ToList(), Color = Colors.HotPink }));
-
-      Trace.WriteLine("Training fitness: " + trainingFitness);
-      Trace.WriteLine("Best test fitness: " + testingFitnessAtEachEpoch.Max());
-
-      //trainingFitness.ShouldEqual(0.875);
+      return RNN.TrainSCG(layers, initialWeights, epochMax, trainingData.Inputs, trainingData.Outputs);
     }
 
     static string Checksum(Vec v)
