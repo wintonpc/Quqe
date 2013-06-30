533c75d3948fb89d3b7034a8a24f29a0cf964c37
Peter Winton <wintonpc@gmail.com>
2013-06-30 12:23:16 -0400

    Removed commented out code

--- QuqeLib/NewVersace/Database.cs
+++ QuqeLib/NewVersace/Database.cs
@@ -10,8 +10,6 @@ namespace Quqe
 {
   public class Database
   {
-    //Dictionary<Type, Dictionary<ObjectId, object>> Lookup = new Dictionary<Type, Dictionary<ObjectId, object>>();
-
     readonly MongoDatabase MDB;
 
     public Database(MongoDatabase db)
@@ -37,48 +35,13 @@ namespace Quqe
 
     public T Get<T>(ObjectId id) where T: MongoTopLevelObject
     {
-      //return Get<T>(id, GetTypeLookup<T>());
       return QueryOne<T>(x => x.Id == id);
     }
 
     public void Store<T>(T value) where T : MongoTopLevelObject
     {
       StoreInMongo(value);
-      //GetTypeLookup<T>().Add(value.Id, value);
     }
-
-    //T Get<T>(ObjectId id, Dictionary<ObjectId, object> typeLookup) where T : MongoTopLevelObject
-    //{
-    //  object value;
-    //  if (!typeLookup.TryGetValue(id, out value))
-    //  {
-    //    value = GetFromMongo<T>(id);
-    //    typeLookup.Add(id, value);
-    //  }
-    //  return (T)value;
-    //}
-
-    //Dictionary<ObjectId, object> GetTypeLookup<T>()
-    //{
-    //  lock (Lookup)
-    //  {
-    //    Dictionary<ObjectId, object> lookup;
-    //    if (!Lookup.TryGetValue(typeof (T), out lookup))
-    //    {
-    //      lookup = new Dictionary<ObjectId, object>();
-    //      Lookup.Add(typeof (T), lookup);
-    //    }
-    //    return lookup;
-    //  }
-    //}
-
-    //T GetFromMongo<T>(ObjectId id) where T : MongoTopLevelObject
-    //{
-    //  var coll = MDB.GetCollection(typeof(T).Name);
-    //  var obj = coll.FindOneAs<T>(Query.EQ("_id", id));
-    //  obj.Database = this;
-    //  return obj;
-    //}
 
     public static Database GetProductionDatabase(string mongoHost)
     {
